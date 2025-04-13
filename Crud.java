1641e57f643b021b20713ecd39fda96803771959
Anshu <101177156+iakumar0132@users.noreply.github.com>
2025-04-13 14:36:22 +0530

    Create Crud.java

---
+++ Crud.java
@@ -0,0 +1,38 @@
+import org.hibernate.*;
+import org.hibernate.cfg.Configuration;
+
+public class StudentDAO {
+    private SessionFactory factory = new Configuration().configure().buildSessionFactory();
+
+    public void createStudent(Student s) {
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.save(s);
+        tx.commit();
+        session.close();
+    }
+
+    public Student getStudent(int id) {
+        Session session = factory.openSession();
+        Student s = session.get(Student.class, id);
+        session.close();
+        return s;
+    }
+
+    public void updateStudent(Student s) {
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        session.update(s);
+        tx.commit();
+        session.close();
+    }
+
+    public void deleteStudent(int id) {
+        Session session = factory.openSession();
+        Transaction tx = session.beginTransaction();
+        Student s = session.get(Student.class, id);
+        session.delete(s);
+        tx.commit();
+        session.close();
+    }
+}
