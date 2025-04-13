96fe2e38c6b0363a066ed4ca2a75e440927e4a89
Anshu <101177156+iakumar0132@users.noreply.github.com>
2025-04-13 14:36:08 +0530

    Create Student2.java

---
+++ Student2.java
@@ -0,0 +1,12 @@
+import jakarta.persistence.*;
+
+@Entity
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private int age;
+
+    // Getters and Setters
+}
