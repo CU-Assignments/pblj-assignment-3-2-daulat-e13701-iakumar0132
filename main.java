3e700a8acb92b06764a70d7cad1550426a1fd1bf
Anshu <101177156+iakumar0132@users.noreply.github.com>
2025-04-13 14:34:55 +0530

    Create main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        Student student = context.getBean(Student.class);
+        student.printDetails();
+        context.close();
+    }
+}
